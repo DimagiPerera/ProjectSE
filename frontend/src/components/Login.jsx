28a385f45ea73ef3677a6b4383159151f552ec52
DimagiPerera <87898809+DimagiPerera@users.noreply.github.com>
2022-08-07 21:04:12 +0530

    Created Login and Registration Components

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { dispatchLogin } from "../redux/actions/authAction";
+import { useDispatch } from "react-redux";
+import "../components/css/Login.css";
+
+export default function Login() {
+  let navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [user, setUser] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setUser((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log("user", user);
+
+    try {
+      const response = await axios.post("user/login", user);
+      alert(response.data.msg);
+      localStorage.setItem("Login", true);
+      console.log("response", response.data.data);
+      localStorage.setItem("LocalUser", JSON.stringify(response.data.data));
+      navigate("/reg");
+
+      dispatch(dispatchLogin());
+    } catch (error) {
+      alert(error.response.data.msg);
+    }
+  };
+  return (
+    <div>
+      <div className="login-box" style={{ marginTop: "10PX" }}>
+        <img
+          src="https://res.cloudinary.com/waste123/image/upload/v1656828180/RMTS/SE/dhq4bo7yviqktcedd4uo.jpg"
+          className="avatar"
+        />
+        <h1>Login Here</h1>
+        <form onSubmit={handleSubmit}>
+          <p>Email</p>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter Email"
+            onChange={handleChange}
+          />
+          <p>Password</p>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            onChange={handleChange}
+          />
+          <button className="btnSubmit" type="submit" name="submit">
+            LOGIN
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,162 @@
+import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+import "../components/css/Login.css";
+import { customAlphabet } from "nanoid";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { isLength, isMatch } from "./validation/Validation";
+const nanoid = customAlphabet("1234567890", 3);
+
+export default function Register() {
+  const auth = useSelector((state) => state.auth);
+  const { LoggedUser } = auth;
+  const [user, setUser] = useState();
+  const navigate = useNavigate();
+  const [token, setToken] = useState();
+  const [localUser, setLocalUser] = useState(
+    JSON.parse(localStorage.getItem("LocalUser"))
+  );
+
+  const redirect = () => {
+    if (!localUser.status) {
+      console.log("21 ", localUser.status);
+      navigate("/");
+    }
+  };
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setUser((values) => ({
+      ...values,
+      [name]: value,
+      id: nanoid(),
+      email: localUser.email,
+      status: false,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log("user", user);
+
+    console.log("token", token);
+
+    const handleLogout = async () => {
+      try {
+        await axios.get("/user/logout", { headers: { Authorization: token } });
+
+        localStorage.removeItem("Login");
+        window.location.href = "/login";
+      } catch (err) {
+        alert(err);
+        window.location.href = "/";
+      }
+    };
+
+    if (isLength(user.password)) {
+      alert("Password must be at least 8 characters long");
+      return;
+    }
+
+    if (!isMatch(user.password, user.cPassword)) {
+      alert("Password and Confirm Password Not Matched");
+      return;
+    }
+
+    try {
+      await axios.post("user/register", user).then((res) => {
+        alert(res.data.msg);
+      });
+
+      await axios
+        .post("user/reset", user, { headers: { Authorization: token } })
+        .then((res) => {
+          alert(res.data.msg);
+          handleLogout();
+        });
+    } catch (error) {
+      alert(error.response.data.msg);
+    }
+  };
+  useEffect(() => {
+    setToken(localStorage.getItem("TOKEN"));
+    redirect();
+  }, []);
+  return (
+    <div>
+      <div className="login-box">
+        <img
+          src="https://res.cloudinary.com/waste123/image/upload/v1656828180/RMTS/SE/dhq4bo7yviqktcedd4uo.jpg"
+          className="avatar"
+        />
+        <h1>Register Here</h1>
+        <form onSubmit={handleSubmit}>
+          <p>First Name</p>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="Enter Firstname"
+            value={localUser.firstName}
+            style={{ cursor: "not-allowed" }}
+            disabled
+          />
+          <p>Last Name</p>
+          <input
+            type="text"
+            name="lastFisrt"
+            placeholder="Enter Lastname"
+            value={localUser.lastName}
+            style={{ cursor: "not-allowed" }}
+            disabled
+          />
+          <p>Email</p>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            value={localUser.email}
+            style={{ cursor: "not-allowed" }}
+            disabled
+          />
+          <p>Date Of Birth</p>
+          <input
+            type="date"
+            name="dateOfBirth"
+            onChange={handleChange}
+            required
+          />
+          <p>Password</p>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            onChange={handleChange}
+            required
+          />
+          <p>Confirm Password</p>
+          <input
+            type="password"
+            name="cPassword"
+            placeholder="Confirm Password"
+            onChange={handleChange}
+            required
+          />
+
+          <p>Mobile Number</p>
+          <input
+            type="tel"
+            name="telephone"
+            pattern="[7-9]{2}-[0-9]{3}-[0-9]{4}"
+            onChange={handleChange}
+          />
+
+          <button className="btnSubmit" type="submit" name="submit">
+            REGISTER
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
