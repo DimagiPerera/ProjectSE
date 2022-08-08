19bf7da31b320de5c43dca44b1338d3ad6a399cd
DimagiPerera <87898809+DimagiPerera@users.noreply.github.com>
2022-08-08 09:13:16 +0530

    Create Home Component

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "../components/CSS/home.css";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import ClipLoader from "react-spinners/ClipLoader";
+
+export default function Home() {
+  const auth = useSelector((state) => state.auth);
+  const { isLogged, isAdmin } = auth;
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <div className="loading">
+      {loading ? (
+        <ClipLoader color={"#ffc500"} loading={loading} size={100} />
+      ) : (
+        <>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="stylesheet" href="style.css" />
+
+          <div className="welcome-text">
+            <h1>
+              MY <span> NOTES</span>
+            </h1>
+            {isLogged ? (
+              isAdmin ? (
+                <a href="/create">CREATE USER</a>
+              ) : (
+                <a href="/list">NOTE LIST</a>
+              )
+            ) : null}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
